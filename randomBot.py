18a2cd7e9ff2af0f0856d51b3b70f3cc3b5f54c5
amelio <mariowii21010@gmail.com>
2020-11-23 14:22:16 +0900

    ヘルプ関連の処理をモジュール化(#1)

--- randomBot.py
+++ randomBot.py
@@ -3,6 +3,8 @@ import random
 import re
 import setting
 
+from lib import help
+
 #メッセージ送信者のボイスチャンネルを取得
 def GetAuthorVChannel(message):
     for server in client.guilds:
@@ -103,24 +105,8 @@ if __name__ == "__main__":
     async def on_message(message):
         if message.author.bot:
             return
-        elif message.content.startswith("/rl -h"):
-            mes = ""
-            mes += "rocket league メンバー集めツール　v1.0 作者：amelio(icemario)\n"
-            mes += ":rl:が6人集まるとメッセージを投稿します\n"
-            mes += "  注意)投稿者はリアクションしてもカウントされません\n"
-            await message.channel.send(mes)
-        elif message.content.startswith('/team -h'):
-            mes = ""
-            mes += "チーム分け支援システム　v1.2 作者：amelio(icemario)\n"
-            mes += "使い方：以下のコマンドをチャット欄に入力\n     /team\n"
-            mes += "説明：自分が居るボイスチャンネルのユーザを2チームに分けます．\n\n"
-            mes += "オプション：\n"
-            mes += "  -num 数字：チーム数を指定．指定が無い場合は2\n"
-            mes += "  -user [文字列]：チーム分けにユーザを追加(-を付けると除外)．スペース区切りで複数追加可能\n"
-            mes += "例) /team -num 4 -user [taro,tanaka,-mike,bob] \n     VC中のユーザにtaro, tanaka, bobを加え，mikeを除いて4チームに分ける"
-            mes += "\n"
-            mes += "  :new:-lol :LoLのカスタムゲーム向けに振り分け\n"
-            await message.channel.send(mes)
+        if help.is_help(message.content):
+            await message.channel.send(help.get_help_mes(message.content))
 
         elif message.content.startswith('/team'):
             vc = GetAuthorVChannel(message)
